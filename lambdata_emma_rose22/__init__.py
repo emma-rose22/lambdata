e811d7574daa5932a6d2117f771acef309ed8f5d
Emma Rose <roseemma324@gmail.com>
2020-01-14 18:31:06 -0500

    fixed class

--- lambdata_emma_rose22/__init__.py
+++ lambdata_emma_rose22/__init__.py
@@ -5,79 +5,83 @@ lambdata- a collection of data science useful functions
 import pandas as pd
 import numpy as np
 
-#sample code
+# sample code
 ONES = pd.DataFrame(np.ones(10))
 ZEROS = pd.DataFrame(np.zeros(50))
 
 
-#first function
+# first function
 '''
-A function to find the mean of a list
+A class to find the mean of a list
 '''
 
-def __init__(self):
-  self.list
 
-def mean(self):
-  return sum(self.list) / len(self.list)
+class Mean:
+
+    def __init__(self, list):
+        self.list
+
+    def mean(self.list):
+        return sum(self.list) / len(self.list)
 
 
-#second function
+# second function
 '''
 a function to replace state names with their abbreviations
 '''
 
-States= (
+States = (
     {'Alabama': 'AL',
-    'Alaska': 'AK',
-    'Arizona': 'AZ',
-    'Arkansas': 'AR',
-    'California': 'CA',
-    'Colorado': 'CO',
-    'Conneticut': 'CT',
-    'Delaware': 'DE',
-    'District of Columbia': 'DC',
-    'Florida': 'FL',
-    'Georgia': 'GA',
-    'Hawaii': 'HI',
-    'Idaho': 'ID',
-    'Illinois': 'IL',
-    'Indiana': 'IN',
-    'Iowa': 'IA',
-    'Kansas': 'KS',
-    'Kentucky': 'KY',
-    'Louisiana': 'LA',
-    'Maine': 'ME',
-    'Maryland': 'MD',
-    'Massachusetts': 'MA',
-    'Michigan': 'MI',
-    'Minnesota': 'MN',
-    'Mississippi': 'MS',
-    'Missouri': 'MO',
-    'Montana': 'MT',
-    'Nebraska': 'NE',
-    'Nevada': ' NV',
-    'New Hampshire': 'NH',
-    'New Jersey': 'NJ',
-    'New Mexico': 'NM',
-    'New York': 'NY',
-    'North Carolina': 'NC',
-    'North Dakota': 'ND',
-    'Ohio': 'OH',
-    'Oklahoma': 'OK',
-    'Oregon': 'OR',
-    'Pennsylvania': 'PA',
-    'Rhode Island': 'RI',
-    'South Carolina': 'SC',
-    'South Dakota': 'SD',
-    'Tennessee': 'TN',
-    'Texas': 'TX',
-    'Utah': 'UT',
-    'Vermont': 'VT',
-    'Washington': 'WA',
-    'West Virginia': 'WV',
-    'Wisconsin': 'WI',
-    'Wyoming': 'WY'})
+     'Alaska': 'AK',
+     'Arizona': 'AZ',
+     'Arkansas': 'AR',
+     'California': 'CA',
+     'Colorado': 'CO',
+     'Conneticut': 'CT',
+     'Delaware': 'DE',
+     'District of Columbia': 'DC',
+     'Florida': 'FL',
+     'Georgia': 'GA',
+     'Hawaii': 'HI',
+     'Idaho': 'ID',
+     'Illinois': 'IL',
+     'Indiana': 'IN',
+     'Iowa': 'IA',
+     'Kansas': 'KS',
+     'Kentucky': 'KY',
+     'Louisiana': 'LA',
+     'Maine': 'ME',
+     'Maryland': 'MD',
+     'Massachusetts': 'MA',
+     'Michigan': 'MI',
+     'Minnesota': 'MN',
+     'Mississippi': 'MS',
+     'Missouri': 'MO',
+     'Montana': 'MT',
+     'Nebraska': 'NE',
+     'Nevada': ' NV',
+     'New Hampshire': 'NH',
+     'New Jersey': 'NJ',
+     'New Mexico': 'NM',
+     'New York': 'NY',
+     'North Carolina': 'NC',
+     'North Dakota': 'ND',
+     'Ohio': 'OH',
+     'Oklahoma': 'OK',
+     'Oregon': 'OR',
+     'Pennsylvania': 'PA',
+     'Rhode Island': 'RI',
+     'South Carolina': 'SC',
+     'South Dakota': 'SD',
+     'Tennessee': 'TN',
+     'Texas': 'TX',
+     'Utah': 'UT',
+     'Vermont': 'VT',
+     'Washington': 'WA',
+     'West Virginia': 'WV',
+     'Wisconsin': 'WI',
+     'Wyoming': 'WY'})
+
 
 def states(list):
-  return [States[x] for x in list if x in(States)] 
+    return [States[x] for x in list if x in(States)]
